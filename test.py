aab040aefc27e83b333fe8b72a1f9bfedc89c7a7
hbh7 <hbh7@hbh7.com>
2020-08-10 15:40:56 -0400

    Added score recording

--- test.py
+++ test.py
@@ -7,22 +7,26 @@ from multiprocessing import Process
 
 def runGame(gameNum):
     env = gym.make('MsPacman-v0')
+    score = 0
     print(env)
     env.reset()
     for k in range(10000):
-        print(env.render(mode='rgb_array'))
+        #print(env.render(mode='rgb_array'))
+        env.render()
         action = env.action_space.sample()
         while action not in [0, 2, 3, 4, 5]:
             action = env.action_space.sample()
         result = env.step(action) # take a random action
-        print("Iteration " + str(k) + ", Action: " + str(action))
+        print("Iteration " + str(k) + ", Action: " + str(action) + ", Current Score: " + str(score))
         #print(result)
         #print(result.observation)
         #print(result.reward)
         #print(result.done)
         #print(result.info)
-        print(result[0])
+        #print(result[0])
         
+        score = score + result[1]
+
         if result[1] > 0:
             print(result[1])
 
@@ -30,7 +34,7 @@ def runGame(gameNum):
             break
 
     env.close()
-    print("Game " + str(gameNum) + " complete")
+    print("Game " + str(gameNum) + " complete, final score: " + str(score))
 
 if __name__ == '__main__':
     for i in range(1):
